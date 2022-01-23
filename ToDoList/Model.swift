0cc88888448c1f01dfbd27a1e91072f8371d3509
Yarchike <yarchike@gmail.com>
2022-01-23 18:44:19 +0300

    16 work

--- ToDoList/Model.swift
+++ ToDoList/Model.swift
@@ -7,29 +7,30 @@
 
 import Foundation
 
-var ToDoItems: [[String: Any]] = [["Name" : "Позвонить маме", "isCompleted" : true],["Name" : "Дописать приложение", "isCompleted" : false],["Name" : "Отменить успешное выполнение задание", "isCompleted" : false]]
-
+var ToDoItems: [[String: Any]] {
+    set{
+        UserDefaults.standard.set(newValue, forKey: "ToDoDataKey")
+        UserDefaults.standard.synchronize()
+    }
+    get{
+        if let array = UserDefaults.standard.array(forKey: "ToDoDataKey") as? [[String: Any]] {
+            return  array
+        }else{
+            return []
+        }
+    }
+}
 func addItem(nameItem: String, isCompeted: Bool = false){
     ToDoItems.append(["Name" : nameItem, "isCompleted" : isCompeted])
-    saveData()
 }
 
 func removeItem(at index: Int){
     ToDoItems.remove(at: index)
-    saveData()
 }
 
-func saveData(){
-    
-}
-
-func loadData(){
-    
-}
 
 func changeState(at item: Int) -> Bool{
     ToDoItems[item]["isCompleted"] = !(ToDoItems[item]["isCompleted"] as! Bool)
-    saveData()
     return  ToDoItems[item]["isCompleted"] as! Bool
 }
 
